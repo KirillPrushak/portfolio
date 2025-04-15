50acaa7c17268d0d3c500e74a0683afc263d5dab
KirillPrushak <kirillprushak@gmail.com>
2025-04-15 14:06:48 +0300

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Home from './components/sections/Home';
 import About from './components/sections/About';
 import Projects from './components/sections/Projects';
 import Contact from './components/sections/Contact';
-import './index.css';
 
 function App() {
     const [isLoaded, setIsLoaded] = useState(false);
@@ -29,10 +28,10 @@ function App() {
                     menuOpen={menuOpen}
                     setMenuOpen={setMenuOpen}
                 ></MobileMenu>
-                <Home></Home>
-                <About></About>
-                <Projects></Projects>
-                <Contact></Contact>
+                <Home />
+                <About />
+                <Projects />
+                <Contact />
             </div>
         </>
     );
